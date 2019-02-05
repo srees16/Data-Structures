d3ed4612b85885abe4574b85893450f74aa7c6f4
sreekanth_concord <sreekanths.dev@auxolabs.in>
2019-02-05 16:25:26 +0530

    Added more methods

--- DataStructures-Algorithms/src/DataStructures/CoreJavaWorkout.java
+++ DataStructures-Algorithms/src/DataStructures/CoreJavaWorkout.java
@@ -623,7 +623,29 @@ public class CoreJavaWorkout {
 			}
 		}
 	}
+	
+	//Compute the average value of an array of integers except the largest and smallest values
+	
+	
+	//Find the difference between the largest and smallest values in an array of integers
+	
+	
+	//Find common elements from three sorted (in non-decreasing order) arrays
+	
+	
+	//Find the number of even and odd integers in a given array of integers
+	
+	
+	//Check if an array of integers contains two specified elements 65 and 77
+	
+	
+	/*Find the length of the longest consecutive elements sequence from a given unsorted array of integers.
+	Sample array: [49, 1, 3, 200, 2, 4, 70, 5]. The longest consecutive elements sequence is [1, 2, 3, 4, 5], therefore the program will return its length 5.*/
 
+	
+	//
+	
+	
 	//The great Main method!
 	public static void main(String[] args) {
 		/*
